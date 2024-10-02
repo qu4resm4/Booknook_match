ff446238e0f419f4e5a06ce1fcad4b0b9bb2c3f3
qu4resm4 <quaresmacont@gmail.com>
2024-10-01 22:01:29 -0300

    mysql
    CRIE O AMBIENTE VIRTUAL
    python -m venv venv
    
    ATIVE-O
    .\venv\Scripts\activate
    
    INSTALE AS DEPENDENCIAS
    pip install -r requirements.txt
    
    ----------------
    PARA RODAR O SERVIDOR
    py run.py
    
    ----------------
    no arquivo run.py
    apagar depois da primeira execução
    db.create_all()

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -7,14 +7,12 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    name = db.Column(db.String(60), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     created_on = db.Column(db.DateTime, default=datetime.datetime.now())
 
     def __init__ (self, username, password, name, email):
         self.username = username
         self.password = password
-        self.name = name
         self.email = email
 
 
--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,5 @@
-from app import app
+from app import app, db
 
 if __name__ == "__main__":
+    db.create_all() # apagar depois da primeira execução
     app.run()
