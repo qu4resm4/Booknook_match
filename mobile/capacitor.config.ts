e4d3d5906c74f7bf1a00c2cd6b647d62f81990fa
qu4resm4 <quaresmacont@gmail.com>
2024-11-29 15:42:56 -0300

    Update capacitor.config.ts

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'mobile',
+  appName: 'Booknook',
   webDir: 'www',
   server: {
     cleartext: true,
